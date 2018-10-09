3379006a380b2df04ca3080eea3566610b6b73c0
TyrannousHail03 <TyrannousHail03@users.noreply.github.com>
2018-10-09 11:45:37 -0400

    v0.2
    Again function is now operational

--- String Comparison.py
+++ String Comparison.py
@@ -1,10 +1,15 @@
 # This was written for a quick checksum check
-
+import sys
 
 def again():
-    print("\nWould you like to do another comparison? ")
-    button = msvcrt.getch()
-
+    again_input = input("\nWould you like to do another comparison? ")
+    if again_input.lower() == "y" or again_input.lower() == "yes":
+        compare()
+    elif again_input.lower() == "n" or again_input.lower() == "no":
+        sys.exit()
+    else:
+        print("Sorry, that was not understood. Please try again.\n")
+        again()
 
 def compare():
     string1 = input("String #1: ")
@@ -14,6 +19,5 @@ def compare():
     else:
         print("Not a match.")
     again()
-
 
 compare()
